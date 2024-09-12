684f65ce1110c1177219e64a2aa93c815d9e79cb
umarllee <gulchohreum20631@sabah.edu.az>
2024-09-12 12:18:57 +0400

    ui

--- src/app/components/module-pages/business-obj-definition/business-obj-definition.component.ts
+++ src/app/components/module-pages/business-obj-definition/business-obj-definition.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { ChangeDetectorRef, Component, Inject, ViewChild } from '@angular/core';
 import { FormGroup, Validators, AbstractControl, FormBuilder } from '@angular/forms';
 import { map, Observable, startWith } from 'rxjs';
 import { NewItemComponent } from './new-item/new-item.component';
@@ -7,6 +7,9 @@ import { filterAutocomplete } from 'src/app/utils/autocomplete';
 import { BusinessService } from 'src/app/services/business.service';
 import { swalSuccess } from 'src/app/utils/alert';
 import { formatDate } from '@angular/common';
+import { SelectionModel } from '@angular/cdk/collections';
+import { PageEvent, MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-business-obj-definition',
@@ -27,7 +30,7 @@ export class BusinessObjDefinitionComponent {
   filteredOptionsunitOwner?: Observable<any[]>;
   filteredOptionsbussFunc?: Observable<any[]>;
   filteredOptionsrole?: Observable<any[]>;
-  
+
   filteredOptionssource_system?: Observable<any[]>;
   filteredOptionssource_system_country_code?: Observable<any[]>;
   filteredOptionsreq_frequency_of_refresh?: Observable<any[]>;
@@ -44,6 +47,32 @@ export class BusinessObjDefinitionComponent {
   filteredOptionsRule?: Observable<any[]>;
   filteredOptionsbusiness_Term?: Observable<any[]>;
 
+  filteredOptioncreated_updated_By?: Observable<any[]>;
+
+  DataOwnerFormGroup!: FormGroup;
+  definitionFormGroup!: FormGroup;
+  SourceSystemFormGroup!: FormGroup;
+  BusinessRulesFormGroup!: FormGroup;
+  BusinessTermFormGroup!: FormGroup;
+  CreatedTermFormGroup!: FormGroup;
+
+  UpdateData: any;
+  isFormValid = true;
+  clients: any[] = [
+    {
+      key: 1,
+      value: 'Test'
+    },
+    {
+      key: 2,
+      value: 'Test 2'
+    },
+    {
+      key: 3,
+      value: 'Meta'
+    },
+  ];
+
   constructor(
     private dialog: MatDialog,
     private fb: FormBuilder,
@@ -51,6 +80,8 @@ export class BusinessObjDefinitionComponent {
   ) {
 
     this.generateForm();
+    this.generateDtOwnerForm();
+
     this.filteredOptionsClient = this.FF['projectName'].valueChanges.pipe(
       startWith(''),
       map((client) => (client ? filterAutocomplete(client, this.clients) : this.clients))
@@ -97,25 +128,24 @@ export class BusinessObjDefinitionComponent {
 
   }
 
-  clients: any[] = [
-    {
-      key: 1,
-      value: 'Test'
-    },
-    {
-      key: 2,
-      value: 'Test 2'
-    },
-    {
-      key: 3,
-      value: 'Meta'
-    },
-  ];
+  ngOnInit() {
+
+  }
+
+  //FORM CODES
+
+  generateDtOwnerForm(){
+    this.DataOwnerFormGroup = this.fb.group({
+      business_unit_owner: [this.UpdateDataDtOwner ? this.UpdateDataDtOwner.business_unit_owner : '', [Validators.required]],
+      business_function: [this.UpdateDataDtOwner ? this.UpdateDataDtOwner.business_function : '', [Validators.required]],
+      role: [this.UpdateDataDtOwner ? this.UpdateDataDtOwner.role : '', [Validators.required]],
+    })
+  }
 
   generateForm() {
     this.definitionFormGroup = this.fb.group({
       id: 0,
-      
+
       dataDomain: '',
       projectName: '',
       business_object_id: ['XXXBO-0001', [Validators.required]],
@@ -129,12 +159,6 @@ export class BusinessObjDefinitionComponent {
       active: ''
     })
 
-    this.DataOwnerFormGroup = this.fb.group({
-      business_unit_owner: ['', [Validators.required]],
-      business_function: ['', [Validators.required]],
-      role: ['', [Validators.required]],
-    })
-    
     this.SourceSystemFormGroup = this.fb.group({
       source_system: ['', [Validators.required]],
       source_system_country_code: ['', [Validators.required]],
@@ -149,27 +173,23 @@ export class BusinessObjDefinitionComponent {
     })
 
     this.BusinessRulesFormGroup = this.fb.group({
+      id: 0,
       ruleId: ['', [Validators.required]],
       rule: ['', [Validators.required]],
     })
 
     this.BusinessTermFormGroup = this.fb.group({
+      id: 0,
       business_term_id: ['', [Validators.required]],
       business_term: ['', [Validators.required]],
     })
-  }
 
-  DataOwnerFormGroup!: FormGroup;
-  definitionFormGroup!: FormGroup;
-  SourceSystemFormGroup!: FormGroup;
-  BusinessRulesFormGroup!: FormGroup;
-  BusinessTermFormGroup!: FormGroup;
-
-  UpdateData: any;
-  isFormValid = true;
-
-  ngOnInit() {
-
+    this.CreatedTermFormGroup = this.fb.group({
+      id: 0,
+      created_updated_By: ['', [Validators.required]],
+      created_updated_ate: [formatDate(new Date(), 'yyyy-MM-dd', 'en'), [Validators.required]],
+      remarks: ['', [Validators.required]],
+    })
   }
 
   get FF(): { [key: string]: AbstractControl } {
@@ -182,6 +202,14 @@ export class BusinessObjDefinitionComponent {
 
   get SSF(): { [key: string]: AbstractControl } {
     return this.SourceSystemFormGroup.controls;
+  }
+
+  get BRF(): { [key: string]: AbstractControl } {
+    return this.BusinessRulesFormGroup.controls;
+  }
+
+  get CRF(): { [key: string]: AbstractControl } {
+    return this.CreatedTermFormGroup.controls;
   }
 
   handleSetAutocomplete(name: string, id: number) {
@@ -197,7 +225,7 @@ export class BusinessObjDefinitionComponent {
 
   item_dialogRef?: MatDialogRef<NewItemComponent>;
 
-  addListValue(){
+  addListValue() {
     this.item_dialogRef = this.dialog.open(NewItemComponent,
       {
         // disableClose: true,
@@ -206,10 +234,10 @@ export class BusinessObjDefinitionComponent {
         height: 'auto',
         autoFocus: false,
         data: {
-        
+
         }
       })
- 
+
     // this.item_dialogRef.afterClosed().subscribe({
     //   next: res => {
     //   }
@@ -232,5 +260,197 @@ export class BusinessObjDefinitionComponent {
       this.isBOFormValid = false;
     }
 
+  }
+
+  //TABLE CODES
+  dataSourceDtOwner: MatTableDataSource<any> = new MatTableDataSource<any>([
+    {
+      business_unit_owner: 'owner',
+      business_function: 'business_function',
+      role: 'role',
+    }
+  ]);
+
+  dataSourceSrcSystem: MatTableDataSource<any> = new MatTableDataSource<any>([
+    {
+      name: 'test source sys',
+      surname: 'test code',
+      testRole: 'test refresh',
+      active: 'y',
+      capmode: 'tets',
+      srcmode: 'tessstt',
+      trackHistory: 'mockdata',
+      historyType: 'mockdata',
+      errTreatment: 'testdata',
+      exepTreatment: 'etstdata',
+    },
+  ]);
+
+  dataSourceBussnRule: MatTableDataSource<any> = new MatTableDataSource<any>([
+    {
+      name: '1',
+      surname: 'test rule',
+    },
+  ]);
+
+  dataSourceAltBusiness: MatTableDataSource<any> = new MatTableDataSource<any>([
+    {
+      name: '1',
+      surname: 'business term',
+
+    },
+  ]);
+
+  activeRowDtOwner: any = -1;
+  activeRowSrcSystem: any = -1;
+  activeRowBussnRule: any = -1;
+  activeRowAltBusiness: any = -1;
+
+  highlightRowDataDtOwner: any;
+  highlightRowDataSrcSystem: any;
+  highlightRowDataBussnRule: any;
+  highlightRowDataAltBusiness: any;
+
+  onChangePageDtOwner(event: PageEvent) {
+  }
+
+  onChangePageSrcSystem(event: PageEvent) {
+
+  }
+
+  onChangePageBussnRule(event: PageEvent) {
+
+  }
+
+  onChangePageAltBusiness(event: PageEvent) {
+
+  }
+
+  isActive = (tableIndex: number, index: number) => { return (tableIndex == 1 ? this.activeRowDtOwner : this.activeRowSrcSystem) === index };
+  isActiveTerm = (tableIndex: number, index: number) => { return (tableIndex == 1 ? this.activeRowBussnRule : this.activeRowAltBusiness) === index };
+
+
+  UpdateDataDtOwner:any;
+  UpdateDataSrsSystem:any;
+
+  highlight(tableIndex: number, index: number, id: number, row: any): void {
+    if (tableIndex == 1) {
+      if (!this.isActive(1, index)) {
+        row != this.highlightRowDataDtOwner ? this.highlightRowDataDtOwner = row : this.highlightRowDataDtOwner = '';
+        this.activeRowDtOwner = index;
+        this.UpdateDataDtOwner = row;
+        this.generateDtOwnerForm();
+      }
+      else {
+        this.activeRowDtOwner = -1;
+        this.highlightRowDataDtOwner = '';
+      }
+    }
+
+    if (tableIndex == 2) {
+      if (!this.isActive(2, index)) {
+        row != this.highlightRowDataSrcSystem ? this.highlightRowDataSrcSystem = row : this.highlightRowDataSrcSystem = '';
+        this.activeRowSrcSystem = index;
+      }
+      else {
+        this.activeRowSrcSystem = -1;
+        this.highlightRowDataSrcSystem = '';
+      }
+    }
+
+  }
+
+  highlightTerm(tableIndex: number, index: number, id: number, row: any): void {
+    if (tableIndex == 1) {
+      if (!this.isActiveTerm(1, index)) {
+        row != this.highlightRowDataBussnRule ? this.highlightRowDataBussnRule = row : this.highlightRowDataBussnRule = '';
+        this.activeRowBussnRule = index;
+      }
+      else {
+        this.activeRowBussnRule = -1;
+        this.highlightRowDataBussnRule = '';
+      }
+    }
+
+    if (tableIndex == 2) {
+      if (!this.isActive(2, index)) {
+        row != this.highlightRowDataAltBusiness ? this.highlightRowDataAltBusiness = row : this.highlightRowDataAltBusiness = '';
+        this.activeRowAltBusiness = index;
+      }
+      else {
+        this.activeRowAltBusiness = -1;
+        this.highlightRowDataAltBusiness = '';
+      }
+    }
+
+  }
+
+  displayedColumnsDtOwner: any = {
+    columns: [
+      'business_unit_owner',
+      'business_function',
+      'role',
+    ],
+    columnsTranslates: ['Business Unit Owner', 'Business Function', 'Role']
+  };
+
+
+  displayedColumnsSrcSystem: any = {
+    columns: [
+      'name',
+      'surname',
+      'testRole',
+      'active',
+      'capmode',
+      'srcmode',
+      'trackHistory',
+      'historyType',
+      'errTreatment',
+      'exepTreatment',
+    ],
+    columnsTranslates: [
+      'Source System',
+      'Source Sys. Country Code',
+      'Req Frequency of Refresh',
+      'Active',
+      'Data Capture Mode',
+      'Sourcing Mode',
+      'Track History ',
+      'History Type',
+      'Error Treatment',
+      'Exception  Treatment',
+    ]
+  };
+
+  displayedColumnsBussnRule: any = {
+    columns: ['name', 'surname',],
+    columnsTranslates: ['Rule Id', 'Rule']
+  };
+
+  displayedColumnsAltBusiness: any = {
+    columns: ['name', 'surname',],
+    columnsTranslates: ['Business Term ID', 'Business Term']
+  };
+
+  pageEvent!: PageEvent;
+  lengthDtOwner?: number;
+  lengthSrcSystem?: number;
+  lengthBussnRule?: number;
+  lengthAltBusiness?: number;
+  pageSize!: number;
+  pageSizeOptions: number[] = [20, 30, 40];
+  @ViewChild('commonPagDtOwner') commonPaginator!: MatPaginator;
+  @ViewChild('commonPagSrcSystem') commonPaginatorSrcSystem!: MatPaginator;
+  @ViewChild('commonPagBussnRule') commonPaginatorBussnRule!: MatPaginator;
+  @ViewChild('commonPagAltBusiness') commonPaginatorAltBusiness!: MatPaginator;
+
+
+
+  //ADD DATA OWNER
+
+  handleAddOwner(){
+    this.dataSourceDtOwner.data.push(this.DataOwnerFormGroup.value);
+    this.dataSourceDtOwner.data = this.dataSourceDtOwner.data;
+    this.generateDtOwnerForm();
   }
 }
